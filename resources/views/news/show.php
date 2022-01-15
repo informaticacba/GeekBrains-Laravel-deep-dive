e30e8f6d8ab41c0cd08e466c7512041a9f0344ef
Pavel <vesna-78d@mail.ru>
2022-01-15 19:31:34 +0300

    completed homework 1 and did the work on lecture 2

---
+++ resources/views/news/show.php
@@ -0,0 +1,6 @@
+<div>
+    <h2><?= $news['title'] ?></h2>
+    <p>Автор: <?= $news['author'] ?>&nbsp; Дата добавления: <?= $news['created_at'] ?></p>
+
+    <p><?= $news['description'] ?></p>
+</div>
